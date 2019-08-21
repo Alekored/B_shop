ea95fadad1f6fc84a69d944b913b89790debde53
Alexander Kovalchuk <redmanynwa888@gmail.com>
2019-08-21 23:24:32 +0300

    responsive_mobile

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import './src/assets/sass/scaffolding.scss'
 import './src/assets/sass/blocks/page-header.scss'
 import './src/assets/sass/blocks/main-nav.scss'
 import './src/assets/sass/blocks/stats.scss'
+import './src/assets/sass/blocks/site-list.scss'
+import './src/assets/sass/blocks/user-list.scss'
+import './src/assets/sass/blocks/advantages.scss'
+import './src/assets/sass/blocks/news.scss'
 
 
 
